0dd6ac074d66f1f343ba893806271873d90b1b86
John Liao <js.liao08@gmail.com>
2015-12-18 23:26:45 -0500

    .1

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
-__author__ = 'johnliao'
+import os
 
-# -*- coding: UTF-8 -*-
-
-# Just importing Forsimatic class
+# Third-party dependencies
+import twitter
 from forismatic import Forismatic
+
+'''
+To do
+Change access tokens to new account
+find python lib to convert text -> image
+create new volume in AWS
+upload and run on chron job
+'''
+
+api = twitter.Api(os.environ.get('TWITTER_KEY'),
+                  os.environ.get('TWITTER_SECRET'),
+                  os.environ.get('TWITTER_TOKEN'),
+                  os.environ.get('TWITTER_SECRET_TOKEN'))
+
+#print api.VerifyCredentials()
+print os.environ.get('TWITTER_KEY'),
 
 # Initializing manager
 f = Forismatic()
