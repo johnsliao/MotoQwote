6fd2304fca81b251a3d5fb08eaddceb192d6202c
John Liao <js.liao08@gmail.com>
2015-12-21 22:39:59 -0500

    formatting

--- main.py
+++ main.py
@@ -9,54 +9,53 @@ from forismatic import Forismatic
 import text2img
 
 def roll():
-    a = random.randint(0,9)
-    if a<5:
+    a = random.randint(0, 9)
+    if a < 5:
         return True
     else:
         return False
 
-logging.basicConfig(format='%(asctime)s %(message)s', filename='quote.log',level=logging.INFO)
-
 # Twitter settings
 twitter = twython.Twython(os.environ.get('TWITTER_KEY'),
-                  os.environ.get('TWITTER_SECRET'),
-                  os.environ.get('TWITTER_ACCESS_TOKEN'),
-                  os.environ.get('TWITTER_ACCESS_STOKEN'))
+                          os.environ.get('TWITTER_SECRET'),
+                          os.environ.get('TWITTER_ACCESS_TOKEN'),
+                          os.environ.get('TWITTER_ACCESS_STOKEN'))
 
 # Initializing manager
 f = Forismatic()
 q = f.get_quote()
 
+logging.basicConfig(format='%(asctime)s %(message)s', filename='quote.log', level=logging.INFO)
 logging.info('Retrieved quote: %s, -%s', q.quote, q.author)
 
 # IMAGE POST TO TWITTER
-if len(q.quote)+len(q.author)+1>140 or roll(): # 1:10 chance to post image or >140 char
+if len(q.quote) + len(q.author) + 1 > 140 or roll():  # 1:10 chance to post image or >140 char
     print 'Image posting...'
-    text2img.text2img(q.quote,q.author)
+    text2img.text2img(q.quote, q.author)
 
     fname = open("quote_img.png", 'rb')
     hashtags = '#motivational #quote '
 
     for name in q.author.split():
-        hashtags+='#'
-        hashtags+=name
-        hashtags+=' '
+        hashtags += '#'
+        hashtags += name
+        hashtags += ' '
 
     print hashtags
 
-    twitter.update_status_with_media(status = hashtags, media = fname)
+    twitter.update_status_with_media(status=hashtags, media=fname)
     print 'complete'
 
 # TEXT ONLY POST TO TWITTER
-# else:
-#     print 'Text posting...'
-#     combined_quote = q.quote
-#     combined_quote += '-'
-#     combined_quote += q.author
-#
-#     if len(combined_quote)+len(" #quote #motivation")<=140: # add hash tags if enough space
-#         combined_quote += " #quote #motivation"
-#     elif len(combined_quote)+len(" #quote")<=140:
-#         combined_quote += " #quote"
-#
-#     twitter.update_status(status=combined_quote)
+else:
+    print 'Text posting...'
+    combined_quote = q.quote
+    combined_quote += '-'
+    combined_quote += q.author
+
+    if len(combined_quote) + len(" #quote #motivation") <= 140:  # add hash tags if enough space
+        combined_quote += " #quote #motivation"
+    elif len(combined_quote) + len(" #quote") <= 140:
+        combined_quote += " #quote"
+
+    twitter.update_status(status=combined_quote)
---
+++ streamer.py
@@ -0,0 +1,19 @@
+import twython
+import os
+
+class TweetStreamer(twython.TwythonStreamer):
+    def on_success(self, data):
+        if 'text' in data:
+            print data['text'].encode('utf-8')
+
+    def on_error(self, status_code, data):
+        print status_code
+        self.disconnect()
+
+streamer = TweetStreamer(os.environ.get('TWITTER_KEY'),
+                          os.environ.get('TWITTER_SECRET'),
+                          os.environ.get('TWITTER_ACCESS_TOKEN'),
+                          os.environ.get('TWITTER_ACCESS_STOKEN'))
+
+streamer.statuses.filter(track = 'motivational')
+
