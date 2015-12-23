72518844bca878ae073d00a0b100e1fa7ba45ab9
John Liao <js.liao08@gmail.com>
2015-12-22 22:51:48 -0500

    hardcoded font loc

--- config.py
+++ config.py
@@ -1,8 +1,10 @@
+#!/usr/bin/python
+
 # text2img Text Config Settings
 CANVAS_HEIGHT=500
 CANVAS_WIDTH=500
 MAX_HEIGHT=400
 MAX_WIDTH=435
-FONT_NAME="Arial.ttf"
+FONT_NAME="./fonts/arial.ttf"
 FONT_SIZE=40
 START_HEIGHT=100
